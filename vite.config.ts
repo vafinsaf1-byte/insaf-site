be25dd63c141337055fb2e7ac412342af1f3be63
vafinsaf1-byte <vafinsaf1@gmail.com>
2026-04-19 13:24:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: vafinsaf1-byte.github.io
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
